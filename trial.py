9b85a40d6c09aef382b03904f432620ca2307863
MapleDTI <itsupport@mapletechnologies.org>
2026-04-16 15:21:33 +0530

    Update trial.py

--- trial.py
+++ trial.py
@@ -60,13 +60,14 @@ users = {
     "chandrakant_sonawane": {"password": "Chandrakant$2025", "name": "Chandrakant Sonawane"},
     "rohit_pujari":{"password": "Rohit#2025", "name": "Rohit Pujari"},
     "madhurima_das":{"password": "Madhurima#2026@", "name": "Madhurima Das"},
+    "ramesh_s":{"password": "Ramesh@TL#26", "name": "Ramesh Subhramani"}
     "sandeep_selvamani": {"password": "Sandeep@2025", "name": "Sandeep Selvamani", "regions": ["Karnataka", "Andhra Pradesh", "Telangana"]},
     "vijay_pandayan": {"password": "Vijay#2026@", "name": "Vijay Pandayan", "regions": ["Tamil Nadu", "Kerala", "Puducherry"]}
 }
 
 # Allowed users for RM filter
 ALLOWED_RM_USERS = ["vishwa_sanghavi", "mahesh_shetty", "sandesh_kadam", "kavish_shah", "manil_shetty","chandrakant_sonawane", "rohit_pujari",
-                   "madhurima_das"]
+                   "madhurima_das","ramesh_s"]
 
 # Regional Managers and their states
 RM_STATES = {
@@ -100,7 +101,7 @@ STATE_MAPPING = {
     'pondicherry': 'Puducherry', 'puducherry': 'Puducherry', 
     'py': 'Puducherry',
     'maharashtra': 'Maharashtra', 'mh': 'Maharashtra',
-    'delhi': 'Delhi'
+    'delhi': 'Delhi',
 }
 
 # File URLs
